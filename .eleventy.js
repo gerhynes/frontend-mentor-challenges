815788e892d6711842e10db567b775edecaaaebd
Gerard Hynes <gkhynes@gmail.com>
2022-06-30 21:36:50 +0100

    Experiment with eleventy-img

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,33 @@
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(src, {
+    widths: [300, 700, null],
+    formats: ["avif", "jpeg"],
+    urlPath: "/images/projects/",
+    outputDir: "./public/images/projects/"
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async"
+  };
+
+  // console.log(metadata);
+
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css/");
   eleventyConfig.addWatchTarget("./src/css/");
   eleventyConfig.addPassthroughCopy("./src/images/");
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+  eleventyConfig.addNunjucksAsyncShortcode("11tyImage", imageShortcode);
 
   eleventyConfig.addCollection(
     "projectsByDifficulty",
